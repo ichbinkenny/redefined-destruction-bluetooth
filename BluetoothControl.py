5c3fe3f38c002cad3ea33e03a005483c1e1a0597
Kenneth Hunter <kennethdhunter@gmail.com>
2020-10-23 19:38:55 +0100

    Added handler to code for movement and attack commands.

--- BluetoothControl.py
+++ BluetoothControl.py
@@ -2,7 +2,7 @@ import bluetooth
 
 uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
 
-def doConnection():
+def doConnection(server_sock):
     client_sock, client_info = server_sock.accept()
     print("Got connection from", client_info)
 
@@ -15,6 +15,7 @@ def doConnection():
             if not data: #no data received
                 break
             print("Received:", data)
+            parseCommand(data.split(' '))
         except:
             connected = False # client disconnected!
 
@@ -22,17 +23,30 @@ def doConnection():
     client_sock.close()
     doConnection() # prevent from closing on dc
 
-print("Creating server socket.")
-server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
-server_sock.bind(("", bluetooth.PORT_ANY))
-server_sock.listen(1)
-# We now need to setup the service that specifies the type of connection we support. Without this, the client wont connect!
-bluetooth.advertise_service(server_sock, "BattleBot Control", service_id=uuid,
+def parseCommand(cmd_list):
+    # For now, this accounts only for joystick position, and attack button status
+    if len(cmd_list) == 2:
+        # Only movements were sent.
+        print("Movement request")
+    elif len(cmd_list) == 3:
+        # Movement and attack sent.
+        print("Movement and attack request")
+
+def setup():
+    print("Creating server socket.")
+    server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
+    server_sock.bind(("", bluetooth.PORT_ANY))
+    server_sock.listen(1)
+    # We now need to setup the service that specifies the type of connection we support. Without this, the client wont connect!
+    bluetooth.advertise_service(server_sock, "BattleBot Control", service_id=uuid,
                             service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
                             profiles=[bluetooth.SERIAL_PORT_PROFILE],
                             # protocols=[bluetooth.OBEX_UUID]
                             )
-print("Waiting for a client to connect...")
-doConnection()
+    print("Waiting for a client to connect...")
+    doConnection(server_sock)
+    # Something forced a return from doConnection, so cleanup the socket.
+    server_sock.close()
 
-server_sock.close()
+if __name__ == "__main__":
+    setup()
