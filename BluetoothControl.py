aed2e0a4efd8a9529a1e36ae9de0f1c0a6661cbd
Kenneth Hunter <kennethdhunter@gmail.com>
2020-12-09 14:29:27 -0700

    Added testing notes from my notebook for easier meeting with Ken tomorrow.

--- BluetoothControl.py
+++ BluetoothControl.py
@@ -1,5 +1,6 @@
 import threading
 import bluetooth
+import doctest
 import subprocess
 from os import system
 import time
@@ -39,6 +40,8 @@ def runUpdateQueue():
                 web_client_proc.stdin.write(stat.encode('utf-8'))
                 web_client_proc.stdin.flush() ## Ensure this makes it to the process!
 
+### Messages are now sent to device based on the received update, with the id tag assisting with resetting bot id.
+### All messages, save for id updates, are now being sent to the parse command!
 def readServerUpdates(client, proc):
     while True:
         msg = proc.stdout.readline().decode('utf-8')
@@ -48,6 +51,12 @@ def readServerUpdates(client, proc):
         print(msg)
         parseStatusUpdate(client, msg)
 
+### ParseStatusUpdate test notes
+# Bots are now receiving their server id!
+# Unknown status messages are being ignore, as appropriate.
+# Only messages consisting of a colon are being handled as commands. This is correct.
+# RDY, BSY, ADD, RMV are all being seen in the client python file!
+# every other message is given to the client file to allow for future growth of commands.
 def parseStatusUpdate(client, msg):
     status_code = -1
     message = ""
@@ -72,6 +81,25 @@ def parseStatusUpdate(client, msg):
     else:
         client.sendall(bytes(msg, 'utf-8'))
 
+
+### Testing for this info is based on phone side responses. 
+### Tests before game started:
+### All Armor disconnected: Expected: 0:0:0 Got: 0:0:0
+### All armor disconnected then armor three added: Expected: 0:0:0 -> 0:0:1 Got: 0:0:0 -> 0:0:1
+### All armor disc then armor two added: Expected: 0:0:0 -> 0:1:0 Got: 0:0:0 -> 0:1:0
+### All armor disc then armor one added: Expected: 0:0:0 -> 1:0:0 Got: 0:0:0 -> 1:0:0
+### All armor disc then armor two and three added: Expected: 0:0:0 -> 0:1:1 Got: 0:0:0 -> 0:1:1
+### All armor disc then all armor added: Expected: 0:0:0 -> 1:1:1 Got: 0:0:0 -> 1:1:1
+### Passing: 6 Failing: 0
+###
+### Tests after game started Begin:
+### All armor disconnected: Expected: Game Over Got: Game Over
+### One Armor Connected: Expected: 1/3 Health Bar Got: 1/3 Health Bar
+### One Armor then Disconnect: Expected 1/3 Health Bar -> Game Over Got: 1/3 Health Bar -> Game Over
+### Two Armor Pieces: Expected: 2/3 Health Bar Got: 2/3 Health Bar
+### Two armor pieces with third added: Expected 2/3 health bar -> 2/3 health bar Got: 2/3 health bar -> 2/3 health bar
+### Three armor -> two armor -> Three armor: Expected: full health bar -> 2/3 health bar Got: Full health bar -> 2/3 health bar
+### Passing: 6 Failing: 0
 def sendArmorStatusToPhone(client_sock, web_client_proc, weapon_proc):
     print("Armor process spawned.")
     serverUpdateThread = multiprocessing.Process(target=readServerUpdates, args=(client_sock, web_client_proc))
@@ -124,6 +152,9 @@ def sendArmorStatusToPhone(client_sock, web_client_proc, weapon_proc):
                 print("UNKNOWN ARMOR STATE: %s" % armor_state[2])
         time.sleep(armor_refresh_rate_ms / 1000.0)
 
+### Do connection test notes
+# All connection data is properly sent to server, received properly handles disconencts by 
+# stopping processes, and resetting device. Afterwards, a new connection is attempted.
 def doConnection(server_sock, web_client_proc, weapon_proc):
     client_sock, client_info = server_sock.accept()
     armor_process = multiprocessing.Process(target=sendArmorStatusToPhone, args=(client_sock,web_client_proc,weapon_proc))
@@ -149,6 +180,13 @@ def doConnection(server_sock, web_client_proc, weapon_proc):
     print("Client disconnected... Awaiting new client connection.")
     client_sock.close()
     doConnection(server_sock, web_client_proc, weapon_proc) # prevent from closing on dc
+
+### Test notes for parse command
+# Combat enter, exit is registered by both the server and the phone! Great.
+# Info for movement, weapon presses, weapon releases all print to console as expected and when expected.
+# Device adding and weapon selection is registered in correct process, and server shows these updates.
+# WPA supplicant file is generated and moved to correct location. Device restart properly puts Pi on new network!
+# Incorrect wpa files are not moved to the /etc/wpa_supplicant/ dir. Good.
 
 def parseCommand(client, cmd_list, web_client_proc=None, weapon_proc=None):
     if len(cmd_list) == 1:
@@ -213,10 +251,29 @@ def parseCommand(client, cmd_list, web_client_proc=None, weapon_proc=None):
             print("Movement and attack request")
 
 def giveWpaSuppStr(ssid, password):
+    """ Returns the formated WPA_SUPPLICANT config
+    >>> giveWpaSuppStr("test", "password")
+    ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\ncountry=US\n\nnetwork={{\n\tssid=\"test\"\n\tpsk=\"password\"\n}}\n
+    >>> giveWpaSuppStr("test", "")
+    Traceback (most recent call last):
+    Exception: Invalid Characters or Password Length.
+    >>> giveWpaSuppStr("", "abcdefghi")
+    Traceback (most recent call last):
+    Exception: Invalid Characters or Password Length
+    >>> giveWpaSuppStr("test", "short")
+    Traceback (most recent call last):
+    Exception: Invalid Characters or Password Length
+    """
     if(len(ssid) < 0 or len(password) < 8):
         raise("Invalid Characters or Password Length.")
     return "ctrl_interface=DIR=/var/run/wpa_supplicant GROUP=netdev\nupdate_config=1\ncountry=US\n\nnetwork={{\n\tssid=\"{}\"\n\tpsk=\"{}\"\n}}\n".format(str(ssid), str(password))
-            
+
+### Setup tests documentation as input is difficult to manipulate.
+# All processes are running 
+# program forcefully closes if connection to server cannot be made. THIS IS INTENDED BEHAVIOR.
+# Bluetooth device is put into correct mode and begins broadcasting. Visible on a plethora of devices.
+# On bluetooth client dc, broadcasting begins again. This is correct.
+# Function is working 100% as intended.     
 def setup():
     print("Putting hci0 into its UP state...")
     system("hciconfig hci0 up")
@@ -245,5 +302,7 @@ def setup():
     server_sock.close()
     web_client_proc.terminate()
 
+
+### This setup runs flawlessly. Tests complete.
 if __name__ == "__main__":
     setup()
